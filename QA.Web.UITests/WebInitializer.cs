a7c6823f0c6489d72fc76ccd5a1e8e0352945b7b
csenoseain <csenoseain@makingsense.com>
2019-01-31 16:30:51 -0300

    RemoteDriver added

--- QA.Web.UITests/WebInitializer.cs
+++ QA.Web.UITests/WebInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using Microsoft.Win32;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
@@ -10,47 +11,80 @@ namespace QA.Web.UITests
 {
     public class WebInitializer
     {
-        public static RemoteWebDriver Driver { get; private set; }
+        public static IWebDriver Driver { get; private set; }
 
-        public RemoteWebDriver Start()
+        public IWebDriver Start()
         {
+            DriverOptions options = null;
             string browser = ConfigurationManager.AppSettings["browser"];
             switch (browser)
             {
                 case "CHROME":
-                    Driver = SetChromeDriver();
+                    options = SetChromeOptions();
                     break;
 
                 case "IE":
-                    Driver = SetIEDriver();
+                    options = SetIEOptions();
                     break;
                 
                 case "FIREFOX":
-                    Driver = SetFirefoxDriver();
+                    options = SetFirefoxOptions();
                     break;
 
                 default:
                     throw new Exception("A driver must be specified in App.config");
             }
+
+            //Common capabilities  
+             DesiredCapabilities capability = new DesiredCapabilities();
+             capability.SetCapability("os", "Windows");
+             capability.SetCapability("os_version", "10");
+            /* capability.SetCapability("browser", "Firefox");
+             capability.SetCapability("browser_version", "65.0 beta");*/
+            /* capability.SetCapability("browser", "Chrome");
+             capability.SetCapability("browser_version", "71.0");
+             */
+             capability.SetCapability("resolution", "1280x1024");
+             capability.SetCapability("project", "QA_Web_UITests");
+             capability.SetCapability("build", "Build 1.0");
+             capability.SetCapability("name", "BasicTest");
+             capability.SetCapability("browserstack.local", "false");
+             capability.SetCapability("browserstack.selenium_version", "3.5.2");
+             capability.SetCapability("browserstack.user", "sergiomarchetti1");
+             capability.SetCapability("browserstack.key", "YmPqJpVnLiYJEeR4yxdR");
+
+
+            string browserstackUrl = "http://hub-cloud.browserstack.com/wd/hub/";
+            Driver = new RemoteWebDriver(new Uri(browserstackUrl), capability);
+            Driver.Navigate().GoToUrl("http://makingsense.com");
             return Driver;
         }
 
-        private RemoteWebDriver SetChromeDriver()
+        private DriverOptions SetChromeOptions()
         {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("disable-infobars");
-            Driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), options, TimeSpan.FromSeconds(90));
-            return SetDriverProperties(Driver);
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("disable-infobars");
+            chromeOptions.AddArgument("--disable-plugins");
+
+          /*  chromeOptions.AcceptInsecureCertificates = true;
+            chromeOptions.AddAdditionalCapability("browserstack.user", "sergiomarchetti1");
+            chromeOptions.AddAdditionalCapability("browserstack.key", "YmPqJpVnLiYJEeR4yxdR");
+            chromeOptions.AddAdditionalCapability("version", "70", true);
+            chromeOptions.AddAdditionalCapability("platform", "Windows 10", true);*/
+
+            //   options.AddAdditionalCapability("browserstack.key", "M4xekjhYuaFGxy4fEboz");
+            // Driver = new ChromeDriver(ChromeDriverService.CreateDefaultService(), chromeOptions, TimeSpan.FromSeconds(90));
+            return chromeOptions;//SetDriverProperties(Driver);
         }
 
         private RemoteWebDriver SetDriverProperties(RemoteWebDriver driver)
         {
-            Driver.Manage().Window.Maximize();
-            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
-            return Driver;
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            return driver;
         }
 
-        private RemoteWebDriver SetIEDriver()
+        private DriverOptions SetIEOptions()
         {
             EnableIEProtectedMode();
             InternetExplorerOptions options = new InternetExplorerOptions
@@ -59,17 +93,19 @@ namespace QA.Web.UITests
             };
 
             Driver = new InternetExplorerDriver(InternetExplorerDriverService.CreateDefaultService(), options, TimeSpan.FromSeconds(90));
-            return SetDriverProperties(Driver);
+            return options;// SetDriverProperties(Driver);
         }
 
-        private RemoteWebDriver SetFirefoxDriver()
+        private DriverOptions SetFirefoxOptions()
         {
-            FirefoxOptions options = new FirefoxOptions
+            FirefoxOptions firefoxProfile = new FirefoxOptions
             {
                BrowserExecutableLocation = GetFirefoxBinaryPath()
             };
-            Driver = new FirefoxDriver(FirefoxDriverService.CreateDefaultService(), options, TimeSpan.FromSeconds(60));
-            return SetDriverProperties(Driver);
+            firefoxProfile.SetPreference("plugin.state.flash", 0);
+            
+            //new FirefoxDriver(FirefoxDriverService.CreateDefaultService(), firefoxProfile, TimeSpan.FromSeconds(60));
+            return firefoxProfile;// SetDriverProperties(Driver);
         }
 
         private string GetFirefoxBinaryPath()
